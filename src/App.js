fc7411841eb092a54b5c9729eeab1b38a8ef7195
Muluken Zemed <zmmulu@gmail.com>
2023-09-05 21:10:41 +0300

    Import Modal, Navbar, CartContainer components within App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
+
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
+import Modal from './components/Modal';
+
+import './styles/styles.css';
 
 function App() {
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch]);
+
+  useEffect(() => {
+    dispatch(getCartItems('random'));
+  }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>CRA with template</h1>
-      </header>
-    </div>
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
   );
 }
-
 export default App;
